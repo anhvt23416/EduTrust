49f4daca3d112c870fab36af5ed194d41d6c791c
An Huỳnh Văn Tú <anhvt23416@st.uel.edu.vn>
2025-09-01 15:46:11 +0000

    minor udpate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -122,7 +122,7 @@ export default function Home() {
         </section>
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t bg-card">
-        <p className="text-xs text-muted-foreground">&copy; 2024 EduTrust. Mọi quyền được bảo lưu.</p>
+        <p className="text-xs text-muted-foreground">&copy; 2025 EduTrust. Mọi quyền được bảo lưu.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link href="#" className="text-xs hover:underline underline-offset-4" prefetch={false}>
             Điều khoản Dịch vụ
